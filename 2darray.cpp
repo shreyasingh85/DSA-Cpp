c1e02e7991a349521e8f617b5b861854ab88d470
shreyasingh85 <singhshreyasingh85@gmail.com>
2026-06-24 21:10:27 +0530

    Add files via upload

---
+++ 2darray.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int matrix[4][3];
+    int rows = 4;
+    int cols = 3;
+    
+    // input
+
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<cols; j++){
+            cin >> matrix[i][j];
+        }
+    } 
+
+    // output
+
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<cols; j++){
+            cout << matrix[i][j]<<" ";
+        
+            }
+        cout << endl;
+    }
+   
+    return 0;
+}
